35cf97baac87feca43b3348ec92e62371e2169b2
Felipe Maeda <maedaxd@gmail.com>
2021-11-09 21:41:02 -0300

    Update treino.py

--- models/treino.py
+++ models/treino.py
@@ -25,6 +25,7 @@ class Exercicio(db.Model):
 
 class Ficha_exercicio(db.Model):
     __tablename__ = "ficha_exercicio"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     id_ficha = db.Column(db.Integer, db.ForeignKey('ficha.id'), nullable=False)
     id_exercicio = db.Column(db.Integer, db.ForeignKey('exercicio.id'), nullable=False)
     carga = db.Column(db.Integer)
@@ -34,14 +35,17 @@ class Ficha_exercicio(db.Model):
 class Serie(db.Model):
     __tablename__ = "serie"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    nm_maquina = db.Column(db.Integer)
     serie = db.Column(db.Integer)
-    repeticoes = db.Column(db.Integer) # 1 = True, 0 = false.  3x15
+    id_aluno = db.Column(db.Integer, db.ForeignKey('aluno.id'))
+    sensorgiroscopio = db.Column(db.Integer)
+    repeticoes = db.Column(db.Integer)
     data = db.Column(db.DateTime)
 
 class Treino(db.Model):
     __tablename__ = "treino"
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    id_aluno = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
+#    id_aluno = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
     id_professor = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
     id_ficha = db.Column(db.Integer, db.ForeignKey('ficha.id'), nullable=False)
     id_musculo = db.Column(db.Integer, db.ForeignKey('musculo.id'), nullable=False)
