fd32ac955940471ea78809f93e589e17efcc9393
Felipe Maeda <maedaxd@gmail.com>
2021-11-12 18:13:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ def protected():
 
 @app.route("/training/stats", methods=["GET"])
 @jwt_required()
-def protected():
+def trainingStatus():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="443")
